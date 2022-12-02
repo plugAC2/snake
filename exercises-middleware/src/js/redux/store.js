8a9093caeb188c4be38b9a79220fca5521650a50
Mateusz Lisiak <70848069+plugAC2@users.noreply.github.com>
2022-12-02 23:18:36 +0100

    Delete exercises-middleware directory

--- exercises-middleware/src/js/redux/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
-import combineReducer from "./reducers/combineReducer";
-import quoteReducer from "./reducers/quoteReducer";
-
-const store = createStore(quoteReducer, applyMiddleware(thunk));
-
-export default store;
